821a3676365ab55f5410e120a6dd9e0456c92d24
lizhigao <lizhigao@021.com>
2021-03-01 10:22:53 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+// import About from '../views/About.vue';
 
 Vue.use(VueRouter);
+
+//导出工厂函数
+// export function createRouter() {
+//   return new VueRouter({
+//     routes: [
+//       { path: '/', component: Home },
+//       { path: '/about', component: About }
+//     ]
+//   });
+// }
 
 const routes = [
   {
--- src/server/ssr.js
+++ src/server/ssr.js
@@ -1,28 +1,38 @@
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
-const Vue = require('vue');
-const { createRenderer } = require('vue-server-renderer');
-
 const app = express();
 
-// 获取渲染器
-const renderer = createRenderer();
+function resolve(dir) {
+  return path.resolve(__dirname, dir);
+}
 
-const vm = new Vue({
-  data() {
-    return {
-      name: 'cleam lee'
-    };
-  },
-  template: `
-    <div>
-      <h1>{{name}}</h1>
-    </div>
-  `
+// 1 开放dist/client目录，关闭默认下载index页的选项，不然到不了后面路由
+app.use(express.static(resolve('../../dist/client'), { index: false }));
+
+// 2 获得一个createBundleRenderer
+const { createBundleRenderer } = require('vue-server-renderer');
+
+// 3 导入服务器打包文件
+const bundle = require(resolve('../../dist/server/vue-ssr-server-bundle.json'));
+
+// 4 创建渲染器
+const template = fs.readFileSync(resolve('../../public/index.html'), 'utf8');
+const clientManifest = require(resolve('../../dist/client/vue-ssr-client-manifest.json'));
+const renderer = createBundleRenderer(bundle, {
+  runInNewContext: false, // https://ssr.vuejs.org/zh/api/#runinnewcontext
+  template,
+  clientManifest
 });
 
-app.get('/', async function(req, res) {
+app.get('*', async function(req, res) {
+  console.log(req.url);
+  const context = {
+    title: 'sst test',
+    url: req.url // 首屏地址
+  };
   try {
-    const html = await renderer.renderToString(vm);
+    const html = await renderer.renderToString(context);
     res.send(html);
   } catch (error) {
     res.status(500).send('Internal Server Error');
@@ -30,5 +40,5 @@ app.get('/', async function(req, res) {
 });
 
 app.listen(3000, () => {
-  console.log('启动成功');
+  console.log(`server started at http://localhost:3000`);
 });
